729484755038525618da776e8c6a3326193ecbe0
Brooks Becton <broabect@ut.utm.edu>
2017-03-12 14:43:54 -0500

    Temp fix for relative file includes.

--- components/footer/footer.php
+++ components/footer/footer.php
@@ -2,3 +2,34 @@
     footer here
 </footer>
 </body>
+<script>
+    $(document).ready(function() {
+
+    var lock = new Auth0Lock(AUTH0_CLIENT_ID, AUTH0_DOMAIN, {
+        auth: {
+            redirectUrl: AUTH0_CALLBACK_URL,
+            responseType: 'code',
+            params: {
+                scope: 'openid'
+            }
+        }
+    });
+
+    $('.btn-login').click(function(e) {
+        e.preventDefault();
+        lock.show();
+    });
+
+    $('.btn-logout').click(function(e) {
+        e.preventDefault();
+        console.log("logout");
+        window.location = 'logout.php';
+    });
+});
+</script>
+<style>
+footer {
+    bottom: 0px;
+    position: absolute;
+}
+</style>
--- components/header/header.php
+++ components/header/header.php
@@ -1,23 +1,3 @@
-    <head>
-        <script src="http://code.jquery.com/jquery-3.1.0.min.js" type="text/javascript"></script>
-        <script src="http://cdn.auth0.com/js/lock/10.2/lock.min.js"></script>
-
-        <script type="text/javascript" src="//use.typekit.net/iws6ohy.js"></script>
-        <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
-
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <!-- font awesome from BootstrapCDN -->
-        <script src="https://npmcdn.com/tether@1.2.4/dist/js/tether.min.js"></script>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>        <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" rel="stylesheet">
-
-        <script src="public/app.js"> </script>
-        <link href="public/app.css" rel="stylesheet">
-    </head>
-
-    <body class="home">
-
     <?php
 
     // Require composer autoloader
@@ -25,6 +5,8 @@
     require __DIR__ . '/../../dotenv-loader.php';
 
     use Auth0\SDK\API\Authentication;
+
+    define('root', $_SERVER['DOCUMENT_ROOT'] . getenv('env'));
 
     $domain        = getenv('AUTH0_DOMAIN');
     $client_id     = getenv('AUTH0_CLIENT_ID');
@@ -43,11 +25,28 @@
     ?>
 
     <?php 
-    require __DIR__ . '/../../db/User.php';
+    require root . 'db/User.php';
     ?>
     <script>
         var AUTH0_CLIENT_ID = '<?php echo getenv('AUTH0_CLIENT_ID'); ?>';
         var AUTH0_DOMAIN = '<?php echo getenv('AUTH0_DOMAIN'); ?>';
         var AUTH0_CALLBACK_URL = '<?php echo getenv('AUTH0_CALLBACK_URL'); ?>';
     </script>
+        <head>
+        <script src="http://code.jquery.com/jquery-3.1.0.min.js" type="text/javascript"></script>
+        <script src="http://cdn.auth0.com/js/lock/10.2/lock.min.js"></script>
+
+        <script type="text/javascript" src="//use.typekit.net/iws6ohy.js"></script>
+        <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- font awesome from BootstrapCDN -->
+        <script src="https://npmcdn.com/tether@1.2.4/dist/js/tether.min.js"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>        <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" rel="stylesheet">
+
+    </head>
+
+    <body class="home">
     <?php include_once("nav.php");?>
